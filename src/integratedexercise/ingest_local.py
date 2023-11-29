fef292dbcf7203ebb37deb841e19641c0b657b8c
Pauline Depuydt <pauline.depuydt@axxes.com>
2023-11-29 14:18:23 +0000

    update

--- src/integratedexercise/ingest_local.py
+++ src/integratedexercise/ingest_local.py
@@ -1,3 +1,4 @@
+
 import sys
 from time import sleep
 import argparse
@@ -11,14 +12,15 @@ pd.set_option('display.max_columns', 20)
 
 api_stations = f"https://geo.irceline.be/sos/api/v1/stations/?expanded=false"
 api_stations_exp = "https://geo.irceline.be/sos/api/v1/stations/?expanded=true"
-api_stations_gent = f"https://geo.irceline.be/sos/api/v1/stations/1207"
+api_stations_gent = f"https://geo.irceline.be/sos/api/v1/stations/1207?expanded=true"
 
 def data_from_api(url, norm):
-    response = requests.get(url)
-    responsej = response.json()
-    print(responsej[0])
-    if norm: df = pd.json_normalize(responsej)
-    else: df = pd.DataFrame(responsej)
+    response_status = requests.get(url)
+    response = response_status.json()
+    if isinstance(response, dict): print(response)
+    else: print(response[0])
+    if norm: df = pd.json_normalize(response)
+    else: df = pd.DataFrame(response)
     return df
 
 
@@ -29,14 +31,25 @@ station_ids_of_interest = stations_of_interest["properties.id"]
 data_from_api(api_stations_exp, norm=False)
 
 # TODO: met timespan parameter telkens de data opvragen van deze dag en wegschrijven, oorspronkelijk was wel de bedoeling om json weg te schrijven
+# get timeseries ids for parameters of interest from station gent
+station_gent = data_from_api(api_stations_gent, norm=False)
+timeseries_gent = pd.DataFrame(station_gent.loc["timeseries", "properties"]).transpose().reset_index(names="timeseries_id")
 
-# get timeseries ids for parameters of intereset from station gent
-data_from_api(api_stations_gent, norm=False)
+# get timeseries metadata for those timeseries available in station of interest
+response_status = requests.get(f"https://geo.irceline.be/sos/api/v1/timeseries/{timeseries_gent.timeseries_id[0]}")
+response = response_status.json()
+print(response)
+timeseries_meta = pd.json_normalize(response)  # is 1 rij dus hiervan kan ik alle timeseries onder elkaar plakken uiteindelijk
 
 
-response = requests.get("https://geo.irceline.be/sos/api/v1/timeseries/10987/?category=71")
-responsej = response.json()
-print(responsej)
-df = pd.DataFrame(responsej)
+"https://geo.irceline.be/sos/api/v1/timeseries/7087"
+# get timeseries datapoints
+timespan = "?timespan=PT24H/2023-11-27"
+#timespan = ""
+response_status = requests.get(f"https://geo.irceline.be/sos/api/v1/timeseries/{timeseries_gent.timeseries_id[0]}/getData{timespan}")
+response = response_status.json()
+df = pd.DataFrame(response)
 df = pd.DataFrame(df['values'].values.tolist())
-print(df.head(20))
+len(df)
+df
+
